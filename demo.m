a359b4dd5055c6813923b2f2fb46f8946c0d43c4
Aarav Jain <jainaarav2712@gmail.com>
2025-09-26 03:24:14 +0530

    dynamic frequency mapping

--- demo.m
+++ demo.m
@@ -1,19 +1,49 @@
 function out = ultrasonic_sim(msg)
-    fs = 48000;
-    T = 0.1;
-    t = 0:1/fs:T-1/fs;
+    % ULTRASONIC_SIM Encode a message into ultrasonic frequencies and decode it back
+    % Input:  msg (string or char array)
+    % Output: formatted string with Encoded, Frequencies, and Decoded message
 
-    digitMap = containers.Map({'1','2','3','4','5','6','7','8','9'}, ...
-        [19000 19250 19500 19750 20000 20250 20500 20750 21000]);
+    % Parameters
+    fs = 48000;        % Sampling frequency (Hz)
+    T = 0.1;           % Duration of each tone (s)
+    t = 0:1/fs:T-1/fs; % Time vector
+    
+    % Define allowed characters
+    charset = ['A':'Z' 'a':'z' '0':'9' ' ' '.' ',' '!' '?' '-' '_' '@' '#'];
+    nChars  = length(charset);
 
+    % Frequency mapping (linear)
+    fStart = 18000;        % Starting frequency (Hz)
+    fStep  = 100;          % Step per character (Hz)
+    freqs  = fStart + (0:nChars-1) * fStep; % All possible frequencies
+
+    % Encode
     signal = [];
+    encodedFreqs = [];
     for i = 1:length(msg)
-        if isKey(digitMap, msg(i))
-            freq = digitMap(msg(i));
-            tone = sin(2*pi*freq*t);
-            signal = [signal tone];
+        ch = msg(i);
+        idx = find(charset == ch, 1); % find character position
+        if ~isempty(idx)
+            f = freqs(idx);
+            tone = sin(2*pi*f*t);
+            signal = [signal tone]; %#ok<AGROW>
+            encodedFreqs(end+1) = f; %#ok<AGROW>
+        else
+            warning('Character "%s" not supported, skipping.', ch);
         end
     end
 
-    out = sprintf('✅ Generated %d tones for message %s', length(msg), msg);
+    % Decode (nearest freq lookup)
+    decodedMsg = "";
+    for f = encodedFreqs
+        [~, idx] = min(abs(freqs - f));
+        decodedMsg = decodedMsg + charset(idx);
+    end
+
+    % Output
+    out = sprintf('✅ Encoded: %s\n🎵 Freqs: %s\n🔓 Decoded: %s', ...
+        msg, mat2str(encodedFreqs), decodedMsg);
+
+    % Optional: play the audio (audible demo only)
+    % sound(signal, fs);
 end
